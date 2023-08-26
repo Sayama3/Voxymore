5193d138beceb2100391beb6a58d7827061d1314
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-08-26 14:05:24 +0200

    Add nicer ImGui Vec3 drawing

--- include/Voxymore/Editor/Panels/PropertyPanel.hpp
+++ include/Voxymore/Editor/Panels/PropertyPanel.hpp
@@ -17,17 +17,23 @@ namespace Voxymore::Editor {
         void OnImGuiRender();
     private:
         template<typename T>
-        inline void DrawComponent(const char* name, void(*OnDrawComponent)(T& component))
+        inline void DrawComponent(const std::string& name, void(*OnDrawComponent)(T& component))
         {
             if (m_SelectedEntity.HasComponent<T>())
             {
-                if(ImGui::TreeNodeEx((void*)typeid(T).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "%s", name))
+                if(ImGui::TreeNodeEx((void*)typeid(T).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "%s", name.c_str()))
                 {
                     OnDrawComponent(m_SelectedEntity.GetComponent<T>());
                     ImGui::TreePop();
                 }
             }
         }
+
+        inline static bool DrawVec3Control(const std::string& label, glm::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f)
+        {
+            return DrawVec3ControlAdvanced(label, values, 0.1f, 0.0f, 0.0f, "%.3f", resetValue, columnWidth);
+        }
+        static bool DrawVec3ControlAdvanced(const std::string& label, glm::vec3& values, float step = 0.1f, float min = 0.0f, float max = 0.0f, const std::string& format = "%.3f", float resetValue = 0.0f, float columnWidth = 100.0f);
     private:
         Entity m_SelectedEntity;
         friend class SceneHierarchyPanel;
--- src/Editor/Panels/PropertyPanel.cpp
+++ src/Editor/Panels/PropertyPanel.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Voxymore/Editor/Panels/PropertyPanel.hpp"
+#include "imgui_internal.h"
 
 
 namespace Voxymore::Editor {
@@ -28,16 +29,22 @@ namespace Voxymore::Editor {
 
             DrawComponent<TransformComponent>("Transform Component", [](TransformComponent& transformComponent){
                 glm::vec3 pos = transformComponent.GetPosition();
-                if(ImGui::DragFloat3("Position", glm::value_ptr(pos), 0.1f))
+                if(DrawVec3Control("Position", pos))
+                {
                     transformComponent.SetPosition(pos);
+                }
 
                 glm::vec3 rot = glm::degrees(transformComponent.GetEulerRotation());
-                if (ImGui::DragFloat3("Rotation", glm::value_ptr(rot), 0.1f))
+                if (DrawVec3Control("Rotation", rot))
+                {
                     transformComponent.SetEulerRotation(glm::radians(rot));
+                }
 
                 glm::vec3 scale = transformComponent.GetScale();
-                if(ImGui::DragFloat3("Scale", glm::value_ptr(scale), 0.1f))
+                if(DrawVec3Control("Scale", scale, 1.0f))
+                {
                     transformComponent.SetScale(scale);
+                }
             });
 
             DrawComponent<MeshComponent>("Mesh Component", [](MeshComponent& meshComponent){
@@ -101,6 +108,74 @@ namespace Voxymore::Editor {
             ImGui::Text("Nothing Selected...");
         }
         ImGui::End();
+    }
+
+    bool PropertyPanel::DrawVec3ControlAdvanced(const std::string& label, glm::vec3& values, float step, float min, float max, const std::string& format, float resetValue, float columnWidth) {
+        // ==================== START ====================
+        bool changed = false;
+        ImGui::PushID(label.c_str());
+        ImGui::Columns(2);
+        ImGui::SetColumnWidth(0, columnWidth);
+        ImGui::Text("%s", label.c_str());
+        ImGui::NextColumn();
+
+        ImGui::PushMultiItemsWidths(3, ImGui::CalcItemWidth());
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+
+        float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+        ImVec2 buttonSize(lineHeight + 3.0f, lineHeight);
+
+
+        // ==================== X ====================
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.9f, 0.2f, 0.2f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(1.0f, 0.3f, 0.25f, 1.0f));
+        if(ImGui::Button("X", buttonSize))
+        {
+            values.x = resetValue;
+            changed |= true;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        changed |= ImGui::DragFloat("##X", &values.x, step, min, max, format.c_str());
+        ImGui::PopItemWidth();
+        ImGui::SameLine();
+
+        // ==================== Y ====================
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.7f, 0.2f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.3f, 0.8f, 0.3f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.4f, 0.9f, 0.4f, 1.0f));
+        if(ImGui::Button("Y", buttonSize))
+        {
+            values.y = resetValue;
+            changed |= true;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        changed |= ImGui::DragFloat("##Y", &values.y, step, min, max, format.c_str());
+        ImGui::PopItemWidth();
+        ImGui::SameLine();
+
+        // ==================== Z ====================
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1f, 0.25f, 0.8f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.2f, 0.35f, 0.9f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.3f, 0.45f, 1.0f, 1.0f));
+        if(ImGui::Button("Z", buttonSize))
+        {
+            values.z = resetValue;
+            changed |= true;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        changed |= ImGui::DragFloat("##Z", &values.z, step, min, max, format.c_str());
+        ImGui::PopItemWidth();
+        ImGui::PopStyleVar();
+
+        // ==================== END ====================
+        ImGui::Columns(1);
+        ImGui::PopID();
+        return changed;
     }
 } // Voxymore
 // Editor
