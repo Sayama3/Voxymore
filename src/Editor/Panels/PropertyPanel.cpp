3d068ada1ed389c4465711f0e332180913a75972
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-08-26 14:09:19 +0200

    Add DrawVec2Control

--- src/Editor/Panels/PropertyPanel.cpp
+++ src/Editor/Panels/PropertyPanel.cpp
@@ -177,5 +177,57 @@ namespace Voxymore::Editor {
         ImGui::PopID();
         return changed;
     }
+    bool PropertyPanel::DrawVec2ControlAdvanced(const std::string& label, glm::vec2& values, float step, float min, float max, const std::string& format, float resetValue, float columnWidth) {
+        // ==================== START ====================
+        bool changed = false;
+        ImGui::PushID(label.c_str());
+        ImGui::Columns(2);
+        ImGui::SetColumnWidth(0, columnWidth);
+        ImGui::Text("%s", label.c_str());
+        ImGui::NextColumn();
+
+        ImGui::PushMultiItemsWidths(2, ImGui::CalcItemWidth());
+        ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(0, 0));
+
+        float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+        ImVec2 buttonSize(lineHeight + 3.0f, lineHeight);
+
+        // ==================== X ====================
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.8f, 0.1f, 0.15f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.9f, 0.2f, 0.2f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(1.0f, 0.3f, 0.25f, 1.0f));
+        if(ImGui::Button("X", buttonSize))
+        {
+            values.x = resetValue;
+            changed |= true;
+        }
+        ImGui::PopStyleColor(3);
+
+        ImGui::SameLine();
+        changed |= ImGui::DragFloat("##X", &values.x, step, min, max, format.c_str());
+        ImGui::PopItemWidth();
+        ImGui::SameLine();
+
+        // ==================== Y ====================
+        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.2f, 0.7f, 0.2f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4(0.3f, 0.8f, 0.3f, 1.0f));
+        ImGui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4(0.4f, 0.9f, 0.4f, 1.0f));
+        if(ImGui::Button("Y", buttonSize))
+        {
+            values.y = resetValue;
+            changed |= true;
+        }
+        ImGui::PopStyleColor(3);
+        ImGui::SameLine();
+        changed |= ImGui::DragFloat("##Y", &values.y, step, min, max, format.c_str());
+        ImGui::PopItemWidth();
+
+        // ==================== END ====================
+        ImGui::PopStyleVar();
+        ImGui::Columns(1);
+        ImGui::PopID();
+        return changed;
+    }
+
 } // Voxymore
 // Editor
