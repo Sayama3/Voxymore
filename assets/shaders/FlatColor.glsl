522af00d53e2a4182a22caf212366178a52cea77
Iannis Poquet <39456336+Sayama3@users.noreply.github.com>
2023-09-08 15:03:48 +0200

    Try updating shader (WIP)

--- assets/shaders/FlatColor.glsl
+++ assets/shaders/FlatColor.glsl
@@ -1,29 +1,37 @@
 #define __TYPE_FRAGMENT_SHADER__
-#version 330 core
+#version 450 core
 
 layout(location = 0) out vec4 o_Color;
 
-in vec3 v_Position;
-in vec4 v_Color;
+layout(location = 0) in vec3 v_Position;
+layout(location = 1) in vec4 v_Color;
 
 void main() {
     o_Color = v_Color;
 }
 
 #define __TYPE_VERTEX_SHADER__
-#version 330 core
+#version 450 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec4 a_Color;
 
-uniform mat4 u_ViewProjectionMatrix;
-uniform mat4 u_Transform;
 
-out vec3 v_Position;
-out vec4 v_Color;
+layout(std140, binding = 0) Camera
+{
+    mat4 u_ViewProjectionMatrix;
+};
+
+layout(push_constant) uniform Transform
+{
+    mat4 Transform;
+} u_RendererUniforms;
+
+layout(location = 0)out vec3 v_Position;
+layout(location = 1)out vec4 v_Color;
 
 void main() {
-    gl_Position = u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0);
-    v_Position = (u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0)).xyz;
+    gl_Position = u_Camera.ViewProjectionMatrix * u_RendererUniforms.Transform * vec4(a_Position, 1.0);
+    v_Position = (u_Camera.ViewProjectionMatrix * u_RendererUniforms.Transform * vec4(a_Position, 1.0)).xyz;
     v_Color = a_Color;
 }
--- assets/shaders/TextureShader.glsl
+++ assets/shaders/TextureShader.glsl
@@ -1,33 +1,44 @@
 #define __TYPE_VERTEX_SHADER__
-#version 330 core
+#version 450 core
 
 layout(location = 0) in vec3 a_Position;
 layout(location = 1) in vec2 a_TexCoord;
 layout(location = 2) in vec4 a_Color;
 
-uniform mat4 u_ViewProjectionMatrix;
-uniform mat4 u_Transform;
+layout(std140, binding = 0) Camera
+{
+    mat4 ViewProjectionMatrix;
+} u_Camera;
 
-out vec3 v_Position;
-out vec2 v_TexCoord;
+layout(push_constant) uniform Transform
+{
+    mat4 Transform;
+} u_RendererUniforms;
+
+struct VertexOutput
+{
+    vec2 TexCoord;
+};
+
+
+layout (location = 0) out VertexOutput Output;
 
 void main() {
-    gl_Position = u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0);
-    v_Position = (u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0)).xyz;
-    v_TexCoord = a_TexCoord;
+    gl_Position = u_Camera.ViewProjectionMatrix * u_RendererUniforms.Transform * vec4(a_Position, 1.0);
+    v_Position = (u_Camera.ViewProjectionMatrix * u_RendererUniforms.Transform * vec4(a_Position, 1.0)).xyz;
+    Output.TexCoord = a_TexCoord;
 }
 
 #define __TYPE_FRAGMENT_SHADER__
-#version 330 core
+#version 450 core
 
 layout(location = 0) out vec4 o_Color;
 
-in vec3 v_Position;
-in vec2 v_TexCoord;
+layout (location = 0) in vec2 v_TexCoord;
 
-uniform sampler2D u_Texture;
+layout (binding = 0) uniform sampler2D u_Texture;
 
 void main() {
-//    o_Color = vec4(v_TexCoord, 0.0, 1.0);
+    //    o_Color = vec4(v_TexCoord, 0.0, 1.0);
     o_Color = texture(u_Texture, v_TexCoord);
 }
