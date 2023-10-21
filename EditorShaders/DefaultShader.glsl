9d0bf469b1ae33f37c088190b3438db374eca7f7
Iannis Poquet <ianpo@live.fr>
2023-10-21 18:31:54 +0200

    Add new default scene & upgrade shaders

--- EditorShaders/DefaultShader.glsl
+++ EditorShaders/DefaultShader.glsl
@@ -1,10 +1,46 @@
 #define __TYPE_VERTEX_SHADER__
 #version 450 core
 
-layout(location = 0) in vec3 a_Position;
-layout(location = 2) in vec3 a_Normal;
-layout(location = 1) in vec2 a_TexCoord;
-layout(location = 3) in vec4 a_Color;
+struct TextureInfo
+{
+    int Index;
+    int TexCoord;
+};
+
+struct NormalTextureInfo
+{
+    int Index;
+    int TexCoord;
+    float Scale;
+};
+
+struct OcclusionTextureInfo
+{
+    int Index;
+    int TexCoord;
+    float Strenght;
+};
+
+struct MetallicRoughtness
+{
+    vec4 BaseColorFactor;
+    TextureInfo BaseColorTexture;
+    float MetallicFactor;
+    float RoughtnessFactor;
+    TextureInfo MetallicRoughnessTexture;
+};
+
+struct MaterialParams
+{
+    MetallicRoughtness PbrMetallicRoughness;
+    NormalTextureInfo NormalTexture;
+    OcclusionTextureInfo OcclusionTexture;
+    TextureInfo EmissiveTexture;
+    vec3 EmissiveFactor;
+    int AlphaMode;
+    float AlphaCutoff;
+    int DoubleSided;
+};
 
 layout(std140, binding = 0) uniform Camera
 {
@@ -18,8 +54,33 @@ layout(std140, binding = 1) uniform Model
 
 layout(std140, binding = 2) uniform Lights
 {
-    vec3 lightDir = {0, -1, 0};
+    vec3 lightDir;
 };
+
+layout(std140, binding = 3) uniform MaterialParameters
+{
+    MaterialParams materialParameters;
+};
+
+
+layout(location = 0) in vec3 a_Position;
+layout(location = 1) in vec3 a_Normal;
+layout(location = 2) in vec2 a_TexCoord;
+layout(location = 3) in vec4 a_Color;
+
+layout (location = 0) out vec2 v_TexCoord;
+layout (location = 1) out flat int v_EntityId;
+layout (location = 2) out vec4 v_Color;
+
+void main() {
+    gl_Position = u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0);
+    v_TexCoord = a_TexCoord;
+    v_EntityId = u_EntityId;
+    v_Color = a_Color;
+}
+
+#define __TYPE_FRAGMENT_SHADER__
+#version 450 core
 
 struct TextureInfo
 {
@@ -44,43 +105,43 @@ struct OcclusionTextureInfo
 struct MetallicRoughtness
 {
     vec4 BaseColorFactor;
-    bool HasBaseColorTexture;
     TextureInfo BaseColorTexture;
     float MetallicFactor;
     float RoughtnessFactor;
-    bool HasMetallicRoughnessTexture;
     TextureInfo MetallicRoughnessTexture;
+};
+
+struct MaterialParams
+{
+    MetallicRoughtness PbrMetallicRoughness;
+    NormalTextureInfo NormalTexture;
+    OcclusionTextureInfo OcclusionTexture;
+    TextureInfo EmissiveTexture;
+    vec3 EmissiveFactor;
+    int AlphaMode;
+    float AlphaCutoff;
+    int DoubleSided;
+};
+
+layout(std140, binding = 0) uniform Camera
+{
+    mat4 u_ViewProjectionMatrix;
+};
+layout(std140, binding = 1) uniform Model
+{
+    mat4 u_Transform;
+    int u_EntityId;
+};
+
+layout(std140, binding = 2) uniform Lights
+{
+    vec3 lightDir;
 };
 
 layout(std140, binding = 3) uniform MaterialParameters
 {
-    bool HasRoughness = false;
-    MetallicRoughtness PbrMetallicRoughness;
-    bool HasNormalTexture = false;
-    NormalTextureInfo NormalTexture;
-    bool HasOcclusionTexture = false;
-    OcclusionTextureInfo OcclusionTexture;
-    bool HasEmissiveTexture = false;
-    TextureInfo EmissiveTexture;
-    vec3 EmissiveFactor = {0,0,0};
-    int AlphaMode = 0;
-    float AlphaCutoff = 0.5f;
-    bool DoubleSided = false;
+    MaterialParams materialParameters;
 };
-
-layout (location = 0) out vec2 v_TexCoord;
-layout (location = 1) out flat int v_EntityId;
-layout (location = 2) out vec4 v_Color;
-
-void main() {
-    gl_Position = u_ViewProjectionMatrix * u_Transform * vec4(a_Position, 1.0);
-    v_TexCoord = a_TexCoord;
-    v_EntityId = u_EntityId;
-    v_Color = a_Color;
-}
-
-#define __TYPE_FRAGMENT_SHADER__
-#version 450 core
 
 layout (location = 0) out vec4 o_Color;
 layout (location = 1) out int o_Entity; // -1 = no entity. (for now.)
@@ -89,13 +150,62 @@ layout (location = 0) in vec2 v_TexCoord;
 layout (location = 1) in flat int v_EntityId;
 layout (location = 2) in vec4 v_Color;
 
-//layout (binding = 0) uniform sampler2D u_Texture;
+layout (binding = 0) uniform sampler2D u_Textures[32];
 
-void main() {
-    o_Color = v_Color;
-    //TODO: Add texture... If any.
-    //    o_Color = texture(u_Texture, v_TexCoord);
-    //    float entityColor = v_EntityId / 20.0f;
-    //    o_Color = vec4(entityColor, entityColor, entityColor, 1.0f);
+vec4 SampleTexture(int texIndex)
+{
+    vec4 texColor = vec4(1,0,1,1);
+
+    switch(int(texIndex))
+    {
+        case  0: texColor = texture(u_Textures[ 0], v_TexCoord /** Input.TilingFactor*/); break;
+        case  1: texColor = texture(u_Textures[ 1], v_TexCoord /** Input.TilingFactor*/); break;
+        case  2: texColor = texture(u_Textures[ 2], v_TexCoord /** Input.TilingFactor*/); break;
+        case  3: texColor = texture(u_Textures[ 3], v_TexCoord /** Input.TilingFactor*/); break;
+        case  4: texColor = texture(u_Textures[ 4], v_TexCoord /** Input.TilingFactor*/); break;
+        case  5: texColor = texture(u_Textures[ 5], v_TexCoord /** Input.TilingFactor*/); break;
+        case  6: texColor = texture(u_Textures[ 6], v_TexCoord /** Input.TilingFactor*/); break;
+        case  7: texColor = texture(u_Textures[ 7], v_TexCoord /** Input.TilingFactor*/); break;
+        case  8: texColor = texture(u_Textures[ 8], v_TexCoord /** Input.TilingFactor*/); break;
+        case  9: texColor = texture(u_Textures[ 9], v_TexCoord /** Input.TilingFactor*/); break;
+        case 10: texColor = texture(u_Textures[10], v_TexCoord /** Input.TilingFactor*/); break;
+        case 11: texColor = texture(u_Textures[11], v_TexCoord /** Input.TilingFactor*/); break;
+        case 12: texColor = texture(u_Textures[12], v_TexCoord /** Input.TilingFactor*/); break;
+        case 13: texColor = texture(u_Textures[13], v_TexCoord /** Input.TilingFactor*/); break;
+        case 14: texColor = texture(u_Textures[14], v_TexCoord /** Input.TilingFactor*/); break;
+        case 15: texColor = texture(u_Textures[15], v_TexCoord /** Input.TilingFactor*/); break;
+        case 16: texColor = texture(u_Textures[16], v_TexCoord /** Input.TilingFactor*/); break;
+        case 17: texColor = texture(u_Textures[17], v_TexCoord /** Input.TilingFactor*/); break;
+        case 18: texColor = texture(u_Textures[18], v_TexCoord /** Input.TilingFactor*/); break;
+        case 19: texColor = texture(u_Textures[19], v_TexCoord /** Input.TilingFactor*/); break;
+        case 20: texColor = texture(u_Textures[20], v_TexCoord /** Input.TilingFactor*/); break;
+        case 21: texColor = texture(u_Textures[21], v_TexCoord /** Input.TilingFactor*/); break;
+        case 22: texColor = texture(u_Textures[22], v_TexCoord /** Input.TilingFactor*/); break;
+        case 23: texColor = texture(u_Textures[23], v_TexCoord /** Input.TilingFactor*/); break;
+        case 24: texColor = texture(u_Textures[24], v_TexCoord /** Input.TilingFactor*/); break;
+        case 25: texColor = texture(u_Textures[25], v_TexCoord /** Input.TilingFactor*/); break;
+        case 26: texColor = texture(u_Textures[26], v_TexCoord /** Input.TilingFactor*/); break;
+        case 27: texColor = texture(u_Textures[27], v_TexCoord /** Input.TilingFactor*/); break;
+        case 28: texColor = texture(u_Textures[28], v_TexCoord /** Input.TilingFactor*/); break;
+        case 29: texColor = texture(u_Textures[29], v_TexCoord /** Input.TilingFactor*/); break;
+        case 30: texColor = texture(u_Textures[30], v_TexCoord /** Input.TilingFactor*/); break;
+        case 31: texColor = texture(u_Textures[31], v_TexCoord /** Input.TilingFactor*/); break;
+    }
+    return texColor;
+}
+
+void main()
+{
+    o_Color = materialParameters.PbrMetallicRoughness.BaseColorFactor;
+
+    if(materialParameters.PbrMetallicRoughness.BaseColorTexture.Index > -1)
+    {
+        o_Color = SampleTexture(materialParameters.PbrMetallicRoughness.BaseColorTexture.Index);
+        if(materialParameters.PbrMetallicRoughness.BaseColorTexture.TexCoord > 0)
+        {
+            o_Color = vec4(0.8,0.2,0.3,1.0);
+        }
+    }
+
     o_Entity = v_EntityId;
 }
